ef46c34267690a75f5ef96b4dfa7c0c171ed75ea
Gelcy Niño de Gúzman <milevelasco1503@gmail.com>
2026-04-01 22:27:07 -0500

    Ejercicios 1 y 2 del tema 1 del curso de Python: Asignación de variables y operadores matemáticos.

---
+++ GM_Ejercicio2.py
@@ -0,0 +1,33 @@
+# OPERADORES ARITMÉTICOS
+a, b = 10, 3
+print(a + b)        # 13        ->  suma
+print(a - b)        # 7         ->  resta
+print(a * b)        # 30        ->  multiplicación
+print(a / b)        # 3.33      ->  división
+print(a // b)       # 3         ->  división entera
+print(a % b)        # 1         ->  división entera
+print(a // b)       # 1         ->  módulo (residuo)
+print(a ** b)       # 1000      ->  potencia
+
+# OPERADORES DE COMPARACIÓN
+print(a > b)        # True
+print(a == b)       # False
+print(a != b)       # True
+
+# OPERADORES LÓGICOS
+x = True
+y = False
+print(x and y)  #False
+print(x or y)   #True
+print(not x)    #False
+
+# OPERADORES DE ASIGNACIÓN
+n = 5
+n += 3          # n = n + 3 -> 8
+print(n)
+n -= 2          # n = n - 2 -> 6
+print(n)
+n *= 4          # n = n * 4 -> 24
+print(n)
+n //= 5         # n = n // 5 -> 4
+print(n)
