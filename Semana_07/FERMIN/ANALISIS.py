e9bd7847b0ae5671611af5306601937c2d356154
FERMIN931 <fermin.reyes@laureate.pe>
2026-05-19 16:39:00 -0500

    Delete Semana_07/FERMIN/ANALISIS.py

--- Semana_07/FERMIN/ANALISIS.py
+++
@@ -1,7 +0,0 @@
-cadena = "Análisis de Datos con Python"
-
-palabra1 = cadena[12:17]
-palabra2 = cadena[22:28]
-
-print("Primera palabra extraída:", palabra1)
-print("Segunda palabra extraída:", palabra2)
